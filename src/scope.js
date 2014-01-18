dd829e8be5acd719b3026b24886e10fd44842cd7
Tero Parviainen <tero.parviainen@eficode.fi>
2014-01-18 21:46:39 +0200

    Detecting Replaced or Reordered Items in Arrays

--- src/scope.js
+++ src/scope.js
@@ -80,6 +80,12 @@ Scope.prototype.$watchCollection = function(watchFn, listenerFn) {
           changeCount++;
           oldValue.length = newValue.length;
         }
+        _.forEach(newValue, function(newItem, i) {
+          if (newItem !== oldValue[i]) {
+            changeCount++;
+            oldValue[i] = newItem;
+          }
+        });
       } else {
 
       }
